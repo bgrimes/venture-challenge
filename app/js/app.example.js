299e17622b50efa5b50dbdc26a96bd41ce425eeb
Ben Grimes <bgrimes@gmail.com>
2012-12-18 16:53:32 -0600

    Updating app.js example to include correct routes for template partial urls

--- app/js/app.example.js
+++ app/js/app.example.js
@@ -1,28 +1,31 @@
 'use strict';
 
 /**
- * Copy this file to app.js and change the urls to whatever is appropriate for you
+ * Update the root_url, base_url, api_url AND base_dir
+ * strings to fully setup this app
  */
 
 // Declare app level module which depends on filters, and services
 angular.module('ventureChallenge', ['ventureChallenge.filters', 'ventureChallenge.services', 'ventureChallenge.directives', 'ngCookies', 'ngResource', 'ui']).
 
-  value('root_url', 'http://localhost/venture').
-  value('base_url', 'http://localhost/venture/app').
-  value('api_url',  'http://localhost/venture/api').
+  value('root_url', 'http://localhost/ci-inet-student').
+  value('base_url', 'http://localhost/ci-inet-student/app').
+  value('api_url',  'http://localhost/ci-inet-student/api').
 
   config(['$routeProvider', function($routeProvider) {
+  var base_dir = '/ci-inet-student/app/';
+
   $routeProvider.when('/', {templateUrl:'partials/ventures.html', controller: VentureCtrl});
-  $routeProvider.when('/venture/:ventureId', {templateUrl: 'partials/venture_view.html', controller: VentureCtrl});
+  $routeProvider.when('/venture/:ventureId', {templateUrl: base_dir + 'partials/venture_view.html', controller: VentureCtrl});
   //$routeProvider.when('/campaigns', {templateUrl:'partials/campaigns.html', controller: CampaignCtrl});
   //$routeProvider.when('/campaign/:campaignId', {templateUrl:'partials/campaign.html', controller: CampaignCtrl});
-  $routeProvider.when('/login', {templateUrl:'partials/login.html', controller: LoginCtrl});
+  $routeProvider.when('/login', {templateUrl:base_dir + 'partials/login.html', controller: LoginCtrl});
 
   // Admin routes
-  $routeProvider.when('/admin', {templateUrl: 'partials/admin/admin.html', controller: AdminCtrl});
+  $routeProvider.when('/admin', {templateUrl: base_dir + 'partials/admin/admin.html', controller: AdminCtrl});
 
   // Register path/controller
-  $routeProvider.when('/register', {templateUrl:'partials/register.html', controller: RegisterCtrl});
+  $routeProvider.when('/register', {templateUrl:base_dir + 'partials/register.html', controller: RegisterCtrl});
 
   // Default to '/'
   $routeProvider.otherwise({redirectTo: '/'});
